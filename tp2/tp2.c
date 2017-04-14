d1eabb561946b4209bbe7267da40b8a34814dac6
Anaïs Donzeau <anais.donzeau@ensimag.grenoble-inp.fr>
2017-04-14 16:50:57 +0200

    Estimation du bruit qui marche peut etre pas

--- tp2/tp2.c
+++ tp2/tp2.c
@@ -1,6 +1,18 @@
 #include "pgm.h"
 #include <math.h>
 
+
+double ConvolutionMasque(double** image, double filtre[3][3], int x, int y, int nl, int nc) {
+  double somme=0;
+  for(int i = -1; i <= 1; i++) {
+    for (int j = -1; j <= 1; j++) {
+      somme += image[(x+i+nl)%nl][(y+j+nc)%nc]*filtre[1+i][1+j];
+    }
+  }
+
+  return somme;
+}
+
 double Wt(double **St, int x, int y, double k) {
   double res = (St[x+1][y]-St[x-1][y])*(St[x+1][y]-St[x-1][y]);
   res += (St[x][y+1]-St[x][y-1])*(St[x][y+1]-St[x][y-1]);
@@ -144,13 +156,74 @@ int median_filter(char* imgOrigin, char*imCible, int n){
   return EXIT_SUCCESS;
 }
 
+double EstimationBruit(char* imgOrigin, int t, double p){
+  int nL, nC;
+  unsigned char **imO = NULL;
+  imO = lectureimagepgm(imgOrigin, &nL, &nC);
+  if(imO == NULL){
+    puts("Lecture image impossible");
+  }
+
+  int* histograme = malloc(256*256*sizeof(int));
+  for (int k=0; k<256*256;k++){
+    histograme[k] = 0;
+  }
+  double variance = 0;
+  double n = (double)(2*t+1)*(2*t+1);
+  double moyenne = 0;
+  double** im1 = imuchar2double(imO, nL, nC);
+  double** im2 = alloue_image_double(nL, nC);  // image avec convo
+  double  passe_haut[3][3] = {{0,-1,0},{-1,5,-1},{0,-1,0}};
+  for (int x = 0; x < nL; x++) {
+    for (int y = 0; y < nC; y++) {
+      im2[x][y] = ConvolutionMasque(im1, passe_haut, x, y, nL, nC);
+    }
+  }
+
+
+  for (int x=t; x<nL-t; x++){
+    for (int y=t; y<nC-t; y++){
+      variance = 0;
+      moyenne = 0;
+      //fprintf(stderr, "image %f \n", im2[x][y] );
+      for(int i=x-t; i<x+t; i++){
+        for(int j = y-t; j<y+t; j++){
+          if(im2[i][j] > 255){
+            im2[i][j] = 255;
+          } else if(im2[i][j] <0){
+            im2[i][j] = 0;
+          }
+          variance += im2[i][j]*im2[i][j];
+          moyenne += im2[i][j];
+        }
+      }
+
+      variance = (variance/n);
+      variance -= moyenne*moyenne/(n*n);
+      histograme[(int) variance]++;
+    }
+  }
+  int cumul = histograme[0];
+  int taux = (int) (p*nL*nC/100);
+  int res = 1;
+  while (cumul<taux && res<256*256) {
+    cumul += histograme[res];
+    res++;
+  }
+  return 1.13*res;
+}
+
+
 int main (int ac, char **av) {
   if (ac < 3) {printf("Usage : %s entree sortie \n",av[0]); exit(1); }
-/*  int n = 3;
-  median_filter(av[1], av[2], n);*/
+//  int n = 3;
+//  median_filter(av[1], av[2], n);
 
+  double bruit = EstimationBruit(av[1], 10, 0.5);
+  fprintf(stdout, "bruit : %f \n", bruit);
+/*
   int iteration = 20;
   double k = 3;
-  filtre_adaptatif_recursif(av[1], av[2], iteration, k);
+  filtre_adaptatif_recursif(av[1], av[2], iteration, k);*/
   return EXIT_SUCCESS;
 }
